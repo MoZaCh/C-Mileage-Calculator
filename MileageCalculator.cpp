fc50387267db0f7335637cd33479c110d1e41107
Mohammed Zahed Choudhury <mzchoudhury@outlook.com>
2023-03-28 18:12:24 +0100

    Function declared

--- MileageCalculator.cpp
+++ MileageCalculator.cpp
@@ -1,1 +1,6 @@
 #include "MileageCalculator.h"
+
+double MileageCalculator(int totalMiles, int mpg, double fuelCost)
+{
+	return 0.0;
+}
--- MileageCalculator.h
+++ MileageCalculator.h
@@ -2,3 +2,5 @@
 #include <iostream>
 
 using namespace std;
+
+double MileageCalculator(int, int, double);
