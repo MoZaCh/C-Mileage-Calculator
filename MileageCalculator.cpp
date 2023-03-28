e67703cc6ecd97a792d9706ccfe16599c5a08384
Mohammed Zahed Choudhury <mzchoudhury@outlook.com>
2023-03-28 18:52:26 +0100

    Function defined & gitignore file updated

--- MileageCalculator.cpp
+++ MileageCalculator.cpp
@@ -1,6 +1,11 @@
 #include "MileageCalculator.h"
+#include <cmath>
 
 double MileageCalculator(int totalMiles, int mpg, double fuelCost)
 {
+	double ans = totalMiles / mpg * 4.54609 * fuelCost / 100;
+	double ansRounded = round(ans * 100) / 100;
+	cout << "Fuel Cost: £ " << ans << endl;
+
 	return 0.0;
 }
