85546d8cab5c72bf1b7a0fdd09fc97b614f14461
Mohammed Zahed Choudhury <mzchoudhury@outlook.com>
2023-04-12 11:10:55 +0100

    Function documentation added

--- MileageCalculator.cpp
+++ MileageCalculator.cpp
@@ -1,6 +1,15 @@
 #include "MileageCalculator.h"
 #include <cmath>
 
+/*
+* Mileage Calculator
+*
+* @param totalMiles - Take the totl miles travelled as integer
+* @param mpg - Takes Miles Per Gallon as integer
+* @param fuelCost - Takes the cost of fuel per litre as double
+*
+* @return ans - The total cost of fuel for the given values
+*/
 double MileageCalculator(int totalMiles, int mpg, double fuelCost)
 {
 	double ans = totalMiles / mpg * 4.54609 * fuelCost / 100;
